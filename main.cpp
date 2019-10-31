3bba038c9a7509fedfae2c2f9353302e942c2970
Vatsu919 <44922949+Vatsu919@users.noreply.github.com>
2019-10-31 20:40:47 +0530

    lab 11{Postmidsem lab 4}(Question 2)

--- main.cpp
+++ main.cpp
@@ -1,88 +1,83 @@
-#include <bits/stdc++.h>
-
+#include<bits/stdc++.h>
+#define f(i, a, b) for(int i=a; i<b; i++)
+#define pb push_back
 using namespace std;
 
-int visited[10000][10000];
-int input[10000][10000];
-int COUNT;
-
-
-
-
-
-bool is_valid(int x, int y, int c, int n, int m)
+class graph
 {
-    if (x < n && y < m && x >= 0 && y >= 0)
-    {
-        if (visited[x][y] == false && input[x][y] == c) return true;
-        else return false;
-    }
-    else return false;
+private:
+	int n;
+	list<int> *adj;
+	int *colour;
+public:
+	graph(int n);
+	void addedge(int v, int w);
+	bool isbipartite(int v);
+	bool disconGraph();
+};
+
+graph::graph(int n)
+{
+	this->n = n;
+	adj = new list<int>[n];
+	colour = new int[n];
 }
 
-void BFS(int x, int y, int i, int j, int n, int m)
+void graph::addedge(int v, int w)
 {
-    if (x != y) return;
-    visited[i][j] = 1;
-    COUNT++;
-    int x_move[] = { 0, 0, 1, -1 };
-    int y_move[] = { 1, -1, 0, 0 };
-    for(int u=0;u<4;u++)
-        if (is_valid(i + y_move[u], j + x_move[u], x, n, m))
-            BFS(x, y, i + y_move[u], j + x_move[u], n, m);
+	adj[v].pb(w);
+	adj[w].pb(v);
 }
 
-void reset_visited(int n, int m)
+bool graph::isbipartite(int v)
 {
-    for(int i=0;i<n;i++)
-    {
-        for(int j=0;j<m;j++) visited[i][j] = 0;
-    }
+	colour[v] = 2;
+	list<int> q;
+	q.pb(v);
+	while(!q.empty())
+	{
+		int s = q.front();
+		q.pop_front();
+		for(auto i:adj[s])
+		{
+			if(colour[i] == colour[s]) return false;
+			else if(!colour[i])
+			{
+				if(colour[s] == 2) colour[i] = 1;
+				else colour[i] = 2;
+				q.pb(i);
+			}
+		}
+	}
+	return true;
 }
 
-void resu(int n, int m)
+bool graph::disconGraph()
 {
-    int current_max = INT_MIN, x;
-   for(int i=0;i<n;i++)
-    {
-       for(int j=0;j<m;j++)
-        {
-            if(input[i][j] == 1)
-            {
-                reset_visited(n, m);
-                COUNT = 0;
-                if (j + 1 < m) BFS(input[i][j], input[i][j + 1], i, j, n, m);
-                if (COUNT >= current_max)
-                {
-                    current_max = COUNT;
-                    x = input[i][j];
-                }
-                reset_visited(n, m);
-                COUNT = 0;
-                if (i + 1 < n) BFS(input[i][j], input[i + 1][j], i, j, n, m);
-                if (COUNT >= current_max)
-                {
-                    current_max = COUNT;
-                    x = input[i][j];
-                }
-            }
-        }
-    }
-    if(current_max == 0) current_max++;
-    cout<<current_max<<endl;
+	f(i, 0 ,n)
+	{
+		if(!colour[i] && !isbipartite(i)) return false;
+	}
+	return true;
 }
+
 int main()
 {
-    int n, m, k, a, b;
-    cin>>n>>m>>k;
-
-    while(k--)
-    {
-        cin>>a>>b;
-        input[a-1][b-1] = 1;
-    }
-
-    resu(n, m);
-
-    return 0;
+	int n, m, v, w, x;
+	cin>>n>>m;
+	int y = n;
+	graph g(n+m+1);
+	f(i, 0, m)
+	{
+		cin>>v>>w>>x;
+		if(x%2 == 1) g.addedge(v, w);
+		else
+		{
+			g.addedge(v, y);
+			g.addedge(y, w);
+			y++;
+		}
+	}
+	if(g.disconGraph()) cout<<"NO\n";
+	else cout<<"YES\n";
 }
