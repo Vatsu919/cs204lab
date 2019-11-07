1e342d732c391d14e2f3a97556cde2924df1ed69
Vatsu919 <44922949+Vatsu919@users.noreply.github.com>
2019-11-08 00:27:02 +0530

    postmidsem lab 5

--- main.cpp
+++ main.cpp
@@ -1,83 +1,109 @@
-#include<bits/stdc++.h>
-#define f(i, a, b) for(int i=a; i<b; i++)
-#define pb push_back
+#include <bits/stdc++.h>
+#define ll long long int
+#define M 1000000007
 using namespace std;
 
-class graph
+class Edge
 {
-private:
-	int n;
-	list<int> *adj;
-	int *colour;
-public:
-	graph(int n);
-	void addedge(int v, int w);
-	bool isbipartite(int v);
-	bool disconGraph();
+    public:
+    ll src, dest, weight;
 };
 
-graph::graph(int n)
+class Graph
 {
-	this->n = n;
-	adj = new list<int>[n];
-	colour = new int[n];
+    public:
+    ll V, E;
+    Edge* edge;
+};
+
+Graph* createGraph(ll V, ll E)
+{
+    Graph* graph = new Graph;
+    graph->V = V;
+    graph->E = E;
+    graph->edge = new Edge[E];
+    return graph;
 }
 
-void graph::addedge(int v, int w)
+class subset
 {
-	adj[v].pb(w);
-	adj[w].pb(v);
+    public:
+    ll parent;
+    ll rank;
+};
+
+ll find(subset subsets[], ll i)
+{
+    if (subsets[i].parent != i) subsets[i].parent = find(subsets, subsets[i].parent);
+    return subsets[i].parent;
 }
 
-bool graph::isbipartite(int v)
+void Union(subset subsets[], ll x, ll y)
 {
-	colour[v] = 2;
-	list<int> q;
-	q.pb(v);
-	while(!q.empty())
-	{
-		int s = q.front();
-		q.pop_front();
-		for(auto i:adj[s])
-		{
-			if(colour[i] == colour[s]) return false;
-			else if(!colour[i])
-			{
-				if(colour[s] == 2) colour[i] = 1;
-				else colour[i] = 2;
-				q.pb(i);
-			}
-		}
-	}
-	return true;
+    ll xroot = find(subsets, x);
+    ll yroot = find(subsets, y);
+    if (subsets[xroot].rank < subsets[yroot].rank) subsets[xroot].parent = yroot;
+    else if (subsets[xroot].rank > subsets[yroot].rank) subsets[yroot].parent = xroot;
+    else
+    {
+        subsets[yroot].parent = xroot;
+        subsets[xroot].rank++;
+    }
 }
 
-bool graph::disconGraph()
+int myComp(const void* a, const void* b)
 {
-	f(i, 0 ,n)
-	{
-		if(!colour[i] && !isbipartite(i)) return false;
-	}
-	return true;
+    Edge* a1 = (Edge*)a;
+    Edge* b1 = (Edge*)b;
+    return a1->weight < b1->weight;
+}
+
+ll MaxWeight(Graph* graph)
+{
+	ll ans = 1;
+    ll V = graph->V;
+    Edge result[V];
+    ll e = 0;
+    ll i = 0;
+    qsort(graph->edge, graph->E, sizeof(graph->edge[0]), myComp);
+    subset *subsets = new subset[( V * sizeof(subset) )];
+    for (ll v = 0; v < V; ++v)
+    {
+        subsets[v].parent = v;
+        subsets[v].rank = 0;
+    }
+    while (e < V - 1 && i < graph->E)
+    {
+        Edge next_edge = graph->edge[i++];
+        ll x = find(subsets, next_edge.src);
+        ll y = find(subsets, next_edge.dest);
+        if (x != y)
+        {
+            result[e++] = next_edge;
+            Union(subsets, x, y);
+        }
+    }
+    for (i = 0; i < e; ++i) ans *= (result[i].weight%M);
+    return ans%M;
 }
 
 int main()
 {
-	int n, m, v, w, x;
+	ll n, m;
 	cin>>n>>m;
-	int y = n;
-	graph g(n+m+1);
-	f(i, 0, m)
+	ll x, y, w;
+
+	Graph* graph = createGraph(n, m);
+
+	for (ll i = 0; i < m; ++i)
 	{
-		cin>>v>>w>>x;
-		if(x%2 == 1) g.addedge(v, w);
-		else
-		{
-			g.addedge(v, y);
-			g.addedge(y, w);
-			y++;
-		}
+		cin>>x>>y>>w;
+		graph->edge[i].src = x-1;
+		graph->edge[i].dest = y-1;
+		graph->edge[i].weight = w;
 	}
-	if(g.disconGraph()) cout<<"NO\n";
-	else cout<<"YES\n";
+
+	cout<<MaxWeight(graph);
+
+	return 0;
 }
